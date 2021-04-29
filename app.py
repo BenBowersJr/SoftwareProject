4ca4ba02b171d482a3780a99d94fb17e473da61f
Ben Bowers <benbowersjr.22@gmail.com>
2021-04-29 14:33:30 -0400

    connected app to heroku database

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
 app = Flask(__name__)
+#connecting to heroku database 
+con = psycopg2.connect(
+  host = "ec2-3-217-219-146.compute-1.amazonaws.com",
+  database= "d3duhguvo7sdom",
+  user="hqlekupiwiodgw",
+  password="e02966a8d4c287b73338f72e099e751c240f11ed6434aa6c5d626e1a11cd2b8c"
+)
+#creating cursor
+cur = con.cursor()
+#execting a query
+cur.execute("select * from pizzatoppings")
+#putting that query into a variable
+rows = cur.fetchall()
+#printing those out in the terminal
+for r in rows:
+  print(f"{r}")
 
-## Im still figuring this out.. dont trust the below line
-app.config['DATABASE_URL'] = 'postgres://hqlekupiwiodgw:e02966a8d4c287b73338f72e099e751c240f11ed6434aa6c5d626e1a11cd2b8c@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d3duhguvo7sdom'
-
-db = SQLAlchemy(app)
-
-class menu(db.Model):
-  __tablename__ = 'menu'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(20))
-
-  def __init__(self, name):
-    self.name = name
 
 @app.route('/')
 def homepage():
@@ -32,10 +36,18 @@ def workOrder():
 def registerLogin():
       return render_template("register-login.html")
 
-@app.route('/menu', methods=['POST', 'GET'])
+@app.route('/customer-menu', methods=['POST', 'GET'])
 def menu():
-  return render_template('cmenu.html')
+  if request.method == 'POST':
+    toppings = request.form['toppings']
+    print(toppings)
+    return render_template('cmenu.html')
+  if request.method == 'GET':
+    return render_template('cmenu.html')
 
+#disconnects the database and cursor
+cur.close()
+con.close()
 
 if __name__ == '__main__':
   app.run(debug=True)
